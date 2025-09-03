2d85cce9af6244514cc5a38b5c81d41c47edc44f
Elder Meyer <meyer.sanchez3000@gmail.com>
2025-09-03 11:42:45 -0600

    1.3.1 Update index funct [db connection] Controllers -> Productos.php

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -4,20 +4,15 @@ namespace App\Controllers;
 
 class Productos extends BaseController{
     public function index(){
-        $data = [
-                    "titulo" => "Catalogo de productos",
-                    "productos" => [
-                        ["id" => 11, "nombre" => "Apple MacBook Pro 17\"", "color" => "Silver", "categoria" => "Laptop", "precio" => "$2999"],
-                        ["id" => 16, "nombre" => "Microsoft Surface Pro", "color" => "White", "categoria" => "Laptop PC", "precio" => "$1999"],
-                        ["id" => 32, "nombre" => "Magic Mouse 2", "color" => "Black", "categoria" => "Accessories", "precio" => "$99"],
-                        ["id" => 53, "nombre" => "Google Pixel Phone", "color" => "Gray", "categoria" => "Phone", "precio" => "$799"],
-                        ["id" => 65, "nombre" => "Apple Watch 5", "color" => "Red", "categoria" => "Wearables", "precio" => "$999"],
-                    ]
-                ];
+        $db = \Config\Database::connect(); //conexión db
+
+        $query = $db->query('SELECT codigo, nombre, stock FROM productos');
+        $resultado = $query->getResult(); //para objetos
+        //$resultado = $query->getResultArray(); //para arrays
+
+        $data = [ "titulo" => "Catalogo de productos", "productos" => $resultado ];
+
         return view('productos/index', $data);
-        // return view('plantilla/header', $data).
-        //         view('productos/index', $data).
-        //         view('plantilla/footer');
     }
 
     public function show($id){
