4e9f93d05885d14d6a665589e2c3ee6c36249f5a
Elder Meyer <meyer.sanchez3000@gmail.com>
2025-09-03 19:24:17 -0600

    1.3.101 Add MOdels/ProductosModel.php

---
+++ app/Models/ProductosModel.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Models;
+use CodeIgniter\Model;
+
+class ProductosModel extends Model{
+    protected $table      = 'productos';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array'; // array or object
+    protected $useSoftDeletes = false; // si es falso $deletedField no se usa
+
+    protected $allowedFields = ['codigo', 'nombre', 'stock', 'id_almacen', 'estatus'];
+
+    // Dates
+    protected $useTimestamps = true; // si es false no se usan los campos de fecha
+    protected $dateFormat    = 'datetime'; // datetime, date, int
+    protected $createdField  = 'fecha_alta';
+    protected $updatedField  = 'fecha_modifica';
+    protected $deletedField  = 'fecha_elimina';
+}
+
+?>
