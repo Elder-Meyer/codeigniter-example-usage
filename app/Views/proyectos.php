06a02d5f462da8a62dce85073422aa2fd5220532
Elder Meyer <meyer.sanchez3000@gmail.com>
2025-09-04 18:14:54 -0600

    1.3.107 Add navbar in proyectos.php

--- app/Views/proyectos.php
+++ app/Views/proyectos.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Proyectos</title>
 </head>
 <body>
-    <div>
+    <?php echo $this->include('plantilla/menu.php'); ?>
+    <div class="min-h-screen">
         <h1>Pagina de proyectos (upgrade)</h1>
-        <a href=<?php echo base_url() ?> >Volver a pagina de inicio</a>
+        <a class="underline text-blue-700" href=<?php echo base_url() ?> >Volver a pagina de inicio</a>
         <ul>
             <?php 
                 foreach($proyectos as $proyecto){
-                    echo "<li><a href='" . base_url("proyectos/" . $proyecto['slug']) . "'>" . esc($proyecto['nombre']) . "</a></li>";
+                    echo "<li><a class='underline text-green-600' href='" . base_url("proyectos/" . $proyecto['slug']) . "'>" . esc($proyecto['nombre']) . "</a></li>";
                 }
             ?>
         </ul>
