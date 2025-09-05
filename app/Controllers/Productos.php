b89c63fd7db0975294c4309bde260c7b4316419e
Elder Meyer <meyer.sanchez3000@gmail.com>
2025-09-05 08:16:58 -0600

    1.3.108 Update db connection for Model(ProductosModel) add basic funcion 'find...'

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -2,23 +2,30 @@
 
 namespace App\Controllers;
 
+use App\Models\ProductosModel;
+
 class Productos extends BaseController{
     public function index(){
-        $db = \Config\Database::connect(); //conexión db
+        /*$db = \Config\Database::connect(); //conexión db
 
         $query = $db->query('SELECT codigo, nombre, stock FROM productos');
-        $resultado = $query->getResult(); //para objetos
-        //$resultado = $query->getResultArray(); //para arrays
+        $resultado = $query->getResult(); //para objetos y... $resultado = $query->getResultArray(); //para arrays*/
+
+        // Forma alternativa de obtener los datos, llama al model en el controlador
+        // y usa el método findAll() para obtener todos los registros de la tabla productos
+        $productoModel = new ProductosModel();
+        $resultado = $productoModel->findAll(); // devuelve un array de objetos por defecto
 
         $data = [ "titulo" => "Catalogo de productos", "productos" => $resultado ];
-
         return view('productos/index', $data);
     }
 
     public function show($id){
+        $productoModel = new ProductosModel();
+        $producto = $productoModel->find($id);
         $data = [
             'titulo' => 'Detalle del producto',
-            'id' => $id
+            'producto' => $producto
         ];
         return view('productos/show', $data);
         // return view('plantilla/header', $data)
